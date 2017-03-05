d33e82b4c7f6313ff04dafd789481b39afbdfd6f
Flávia <Flvia@TGH-FL>
2017-03-05 12:22:13 +0000

    Início do desenvolvimento do cenário de Busca.

---
+++ src/test/java/pt/wook/TestesFuncionais/TestesBusca.java
@@ -0,0 +1,21 @@
+package pt.wook.TestesFuncionais;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TestesBusca {
+
+	@Test
+	
+	public void PesquisarLivroExistente(){
+		
+		String nomeLivro = "SOS Ansiedade";
+		boolean exibiuLivro = true;
+		
+		// Obter o estado da tela.
+		
+		assertTrue(exibiuLivro);
+		
+		
+	}
+}
