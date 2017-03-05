c2d273318967f0c9b9c3f6eeb5ca02e8326ed279
Flávia <Flvia@TGH-FL>
2017-03-05 11:51:08 +0000

    Primeiro Commit

---
+++ src/main/java/pt/wook/TestesFuncionais/App.java
@@ -0,0 +1,13 @@
+package pt.wook.TestesFuncionais;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
