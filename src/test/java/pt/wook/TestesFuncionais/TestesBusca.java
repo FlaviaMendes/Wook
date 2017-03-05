a0b2f86cd25e779ffaf8bc1a408cbd900d15a94e
Flávia <Flvia@TGH-FL>
2017-03-05 13:49:17 +0000

    Busca por um livro existente.

--- src/test/java/pt/wook/TestesFuncionais/TestesBusca.java
+++ src/test/java/pt/wook/TestesFuncionais/TestesBusca.java
@@ -2,9 +2,10 @@ package pt.wook.TestesFuncionais;
 
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestesBusca {
 
@@ -18,8 +19,20 @@ public class TestesBusca {
 		boolean exibiuLivro = true;
 		String urlDoSite = "https://www.wook.pt/";
 		
-		WebDriver navegador = new ChromeDriver();
-		navegador.get(urlDoSite);
+		WebDriver navegador = new ChromeDriver(); // abre o navegador sem nada
+		navegador.manage().window().maximize();
+		navegador.get(urlDoSite);// abre o site
+		
+		
+		WebElement boxBuscar = navegador.findElement(By.xpath("//*[@id=\"header-navbar\"]/div[2]/div[1]"));
+		boxBuscar.click();
+		
+		WebElement caixaDeBusca = navegador.findElement(By.id("form-searchform-palavra"));
+		caixaDeBusca.sendKeys(nomeLivro);
+		
+		WebElement botaoBuscar = navegador.findElement(By.xpath("//*[@id=\"header-navbar\"]/div[2]/div[1]/button[1]"));
+		botaoBuscar.click();
+		
 		
 		assertTrue(exibiuLivro);
 		
