cd4161131fa183bd7815af43c7a2a0fba8d11ce6
Flávia <Flvia@TGH-FL>
2017-03-05 12:43:39 +0000

    Abrindo navegador com o site.

--- src/test/java/pt/wook/TestesFuncionais/TestesBusca.java
+++ src/test/java/pt/wook/TestesFuncionais/TestesBusca.java
@@ -2,6 +2,9 @@ package pt.wook.TestesFuncionais;
 
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestesBusca {
 
@@ -9,10 +12,14 @@ public class TestesBusca {
 	
 	public void PesquisarLivroExistente(){
 		
+		System.setProperty("webdriver.chrome.driver", "C:\\EstudoFlavia\\chromedriver.exe");
+		
 		String nomeLivro = "SOS Ansiedade";
 		boolean exibiuLivro = true;
+		String urlDoSite = "https://www.wook.pt/";
 		
-		// Obter o estado da tela.
+		WebDriver navegador = new ChromeDriver();
+		navegador.get(urlDoSite);
 		
 		assertTrue(exibiuLivro);
 		
