e901e383131d2b7b3e2894f34d0d36051df80d70
Flávia <Flvia@TGH-FL>
2017-03-13 15:22:05 +0000

    Correção de teste com erro

--- src/test/java/pt/wook/TestesFuncionais/paginas/PaginaConfirmarRegistro.java
+++ src/test/java/pt/wook/TestesFuncionais/paginas/PaginaConfirmarRegistro.java
@@ -9,18 +9,9 @@ public class PaginaConfirmarRegistro extends PaginaBase{
 		
 		WebElement mensagemRegistroComSucesso = navegador.findElement(By.xpath("//*[@id=\"overlayConfirmRegist\"]/div[2]/h1"));
 		String mensagemRegistroExibida = mensagemRegistroComSucesso.getText();
-		String mensagemRegistroEsperada = "O seu registo foi efetuado com sucesso.";
-		assertEquals(mensagemRegistroEsperada, mensagemRegistroExibida);
-		
-		/*WebElement mensagemExibida = navegador.findElement(By.xpath("//*[@id=\"overlayConfirmRegist\"]/div[2]/h1"));
-		assertTrue(mensagemExibida.isDisplayed());*/
 			
-		return "O seu registo foi efetuado com sucesso.";
+		return mensagemRegistroExibida;
 		
 	}
-
-	
-
-	
 
 }
